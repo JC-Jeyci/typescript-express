ce95bf8b29712ef7cde06a7b73747003092c8888
JC-Jeyci <11jeyci@gmail.com>
2023-03-13 15:36:08 -0600

    creando los tipos y creando nuevas rutas

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,13 +1,19 @@
 import express from 'express'
+import * as diaryServices from '../services/diaryServices'
 
 const router = express.Router()
 
 router.get('/', (_req, res) => {
-    res.send('fetching all entry diaries')
+  res.send(diaryServices.getEntriesWithoutSensitiveInfo())
+})
+
+router.get('/:id', (req, res) => {
+  const diary = diaryServices.findById(+req.params.id)
+  return (diary !== null) ? res.send(diary) : res.sendStatus(404)
 })
 
 router.get('/', (_req, res) => {
-    res.send('Saving a diary')
+  res.send('Saving a diary')
 })
 
 export default router
---
+++ src/services/diaryServices.ts
@@ -0,0 +1,22 @@
+import { DiaryEntry, NonSensitiveInfoDiaryEntry } from '../types'
+import diaryData from './diaries.json'
+
+const diaries: DiaryEntry[] = diaryData as DiaryEntry[]
+
+export const getEntries = (): DiaryEntry[] => diaries
+
+export const findById = (id: number): NonSensitiveInfoDiaryEntry | undefined => {
+  const entry = diaries.find(d => d.id === id)
+
+  return entry
+}
+
+export const getEntriesWithoutSensitiveInfo = (): NonSensitiveInfoDiaryEntry[] => {
+  return diaries.map(({ id, date, weather, visibility }) => {
+    return {
+      id, date, weather, visibility
+    }
+  })
+}
+
+export const addEntry = (): undefined => undefined
